51954c3ea942692f699cb1eb7d691dbcd09324d5
Lord Crawford <lrc2161@columbia.edu>
2023-08-14 03:06:49 -0400

    art

--- src/components/art.js
+++ src/components/art.js
@@ -4,9 +4,26 @@ const Art = () => {
 
     return (
         <div id="art">
-            <div id="selector">all | charcoal | ink | graphite</div>
-            <div id="images">
-                <img src={'./art4website/deepest_corner.jpeg'} alt="Deepest Corner" />
+            <div id="selector" className="mb-8 ml-2 space-x-6">
+                <span>all</span>
+                <span>charcoal</span>
+                <span>ink</span>
+                <span>graphite</span>
+                
+            </div>
+            <div id="images" className="space-y-10 space-x-5 flex flex-wrap mb-10">
+                <img id="deepest_corner" className ="ink" src={require('./art4website/deepest_corner.jpeg')} alt="Deepest Corner" 
+                    width="80%"/>
+                <img id="alter_ego" className ="charcoal" src={require('./art4website/alter_ego.jpeg')} alt="Alter Ego"
+                    width="70%" />
+                <img id="vortex" className ="charcoal" src={require('./art4website/vortex.png')} alt="Vortex" 
+                    width="80%"/>
+                <img id="new_world" className ="ink" src={require('./art4website/new_world.jpeg')} alt="New World" 
+                    width="60%"/>
+                <img id="first" className ="graphite" src={require('./art4website/first.jpeg')} alt="First" 
+                    width="45%"/>
+                <img id="second" className ="graphite" src={require('./art4website/second.jpeg')} alt="Second" 
+                    width="45%"/>
             </div>
         </div>
     )
--- src/components/projects.js
+++ src/components/projects.js
@@ -9,48 +9,48 @@ const Projects = () => {
 
     return (
         <div id="projects">
-            <div id="all_project_list w-4/5 h-full" class="space-y-4 mb-10">
-                <div class="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+            <div id="all_project_list w-4/5 h-full" className="space-y-4 mb-10">
+                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-indigo-300"
                 onClick={(e) => goToLink('https://lordcrawford.github.io/everydaysounds/')}>
-                    <div class="title self-center text-2xl w-2/5 pr-2">Everyday Sounds</div>
-                    <div class="description self-center text-sm w-3/5 pl-3">Web-based beat maker</div>
+                    <div className="title self-center text-2xl w-2/5 pr-2">Everyday Sounds</div>
+                    <div className="description self-center text-sm w-3/5 pl-3">Web-based beat maker</div>
                 </div>
 
-                <div class="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                <div className="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-green-100">
-                    <div class="title self-center text-2xl w-2/5 pr-2">2ofAKind Vending</div>
-                    <div class="description self-center text-sm w-3/5 pl-3"> Fully designed and built website 
+                    <div className="title self-center text-2xl w-2/5 pr-2">2ofAKind Vending</div>
+                    <div className="description self-center text-sm w-3/5 pl-3"> Fully designed and built website 
                     for a vending machine business</div>
                 </div>
 
-                <div class="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-red-300"
                 onClick={(e) => goToLink('https://youtu.be/qeGczrRkh34')}>
-                    <div class="title self-center text-2xl w-2/5 pr-2">VR Cooking Simulator</div>
-                    <div class="description self-center text-sm w-3/5 pl-3 pr-4">Virtual kitchen environment that teaches players how to make pancakes</div>
+                    <div className="title self-center text-2xl w-2/5 pr-2">VR Cooking Simulator</div>
+                    <div className="description self-center text-sm w-3/5 pl-3 pr-4">Virtual kitchen environment that teaches players how to make pancakes</div>
                 </div>
 
-                <div class="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-purple-100"
                 onClick={(e) => goToLink('https://www.marketingdive.com/news/new-york-times-wordle-new-mobile-ad-format-doordash/685073/')}>
-                    <div class="title self-center text-2xl w-2/5 pr-2">Wordle Mobile Ad</div>
-                    <div class="description self-center text-sm w-3/5 pl-3"> Supported the launch of a full-screen interstitial
+                    <div className="title self-center text-2xl w-2/5 pr-2">Wordle Mobile Ad</div>
+                    <div className="description self-center text-sm w-3/5 pl-3"> Supported the launch of a full-screen interstitial
                     advertisement on The New York Times' Wordle game</div>
                 </div>
 
-                <div class="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-orange-100"
                 onClick={(e) => goToLink('https://business.twitter.com/en/help/campaign-setup/campaign-targeting/custom-audiences.html')}>
-                    <div class="title self-center text-2xl w-2/5 pr-2">Audience Sharing</div>
-                    <div class="description self-center text-sm w-3/5 pl-3"> Twitter Ads feature that allows 
+                    <div className="title self-center text-2xl w-2/5 pr-2">Audience Sharing</div>
+                    <div className="description self-center text-sm w-3/5 pl-3"> Twitter Ads feature that allows 
                     advertisers to share audiences amongst each other and more effectively reach their audiences</div>
                 </div>
 
-                <div class="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                <div className="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
                 transition transform hover:skew-y-1 hover:border-l-8 hover:bg-yellow-100">
-                    <div class="title self-center text-2xl w-2/5 pr-2">A Post a Day Keeps the Doctor Away</div>
-                    <div class="description self-center text-sm w-3/5 pl-3"> Human-Computer Interaction paper analyzing the factors 
+                    <div className="title self-center text-2xl w-2/5 pr-2">A Post a Day Keeps the Doctor Away</div>
+                    <div className="description self-center text-sm w-3/5 pl-3"> Human-Computer Interaction paper analyzing the factors 
                         that affect people’s willingness to share personal info on online self-diagnosis platforms</div>
                 </div>
                 
