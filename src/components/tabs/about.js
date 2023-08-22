9ca815eefa45a8e65eca3ca844a72bad88f7f0c3
Lord Crawford <lrc2161@columbia.edu>
2023-08-21 23:44:22 -0400

    small changes

--- src/components/tabs/about.js
+++ src/components/tabs/about.js
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <div id="about" className='text-[3.5vw] md:text-[1.2vw] overflow-hidden'>
             <div className="md:flex md:items-center">
-                <img className="md:w-[25%] md:mr-[4%] md:self-center" src={require('../photos/about_pic.jpeg')} alt="Bio"/>
-                <div className="md:w-[65%] mt-[8%] md:mt-[0%]">
+                <img className="md:w-[25%] w-[95%] md:mr-[4%] md:self-center" src={require('../photos/about_pic.jpeg')} alt="Bio"/>
+                <div className="md:w-[65%] mt-[10%] md:mt-[0%]">
                 Hey, I'm Lord. I created this website as a space for my passions to intermingle, 
                 I hope it inspires others to create a space for their thoughts to live. I'm an 
                 African American born and raised in New York City. I've been titled many things — 
@@ -16,7 +16,7 @@ const About = () => {
                 
             </div> 
 
-            <div className="md:w-[94%]">
+            <div className="mb-[50%] md:w-[94%]">
                 <br/>
                 Currently, I'm a Product Manager at The New York Times. In 2022, I graduated from 
                 Columbia University with a degree in Computer Science and focus on Human-Computer Interaction.
@@ -29,6 +29,7 @@ const About = () => {
                 <br/><br/>
 
                 <span className="font-black">for consulting and personal: </span><a className="underline decoration-blue-500 decoration-2" href="mailto:lordcrawford2@gmail.com">lordcrawford2@gmail.com</a>
+
             </div>  
         </div>
     )
