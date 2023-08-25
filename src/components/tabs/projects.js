dc2557188c62453ccbe019f3de51f00518f1e8db
Lord Crawford <lrc2161@columbia.edu>
2023-08-25 03:53:00 -0400

    small fix on projects page

--- src/components/tabs/projects.js
+++ src/components/tabs/projects.js
@@ -10,11 +10,18 @@ const Projects = () => {
     return (
         <div id="projects">
             <div id="all_project_list w-4/5 h-full" className="space-y-4 mb-10">
-                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
-                transition transform hover:skew-y-1 hover:border-l-8 hover:bg-indigo-300"
-                onClick={(e) => goToLink('https://lordcrawford.github.io/everydaysounds/')}>
+                <div className="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                transition transform hover:skew-y-1 hover:border-l-8 hover:bg-indigo-300">
                     <div className="title self-center w-2/5 pr-2">Everyday Sounds</div>
-                    <div className="description self-center w-3/5 pl-3">Web-based beat maker</div>
+                    <div className="description self-center w-3/5 pl-3">Web-based beat maker ~ in-progress ~</div>
+                </div>
+
+                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
+                transition transform hover:skew-y-1 hover:border-l-8 hover:bg-purple-100"
+                onClick={(e) => goToLink('https://www.marketingdive.com/news/new-york-times-wordle-new-mobile-ad-format-doordash/685073/')}>
+                    <div className="title self-center w-2/5 pr-2">Wordle Mobile Ad</div>
+                    <div className="description self-center w-3/5 pl-3"> Supported the launch of a full-screen interstitial
+                    advertisement on The New York Times' Wordle game</div>
                 </div>
 
                 <div className="project flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
@@ -29,14 +36,6 @@ const Projects = () => {
                 onClick={(e) => goToLink('https://youtu.be/qeGczrRkh34')}>
                     <div className="title self-center w-2/5 pr-2">VR Cooking Simulator</div>
                     <div className="description self-center w-3/5 pl-3 pr-4">Virtual kitchen environment that teaches players how to make pancakes</div>
-                </div>
-
-                <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
-                transition transform hover:skew-y-1 hover:border-l-8 hover:bg-purple-100"
-                onClick={(e) => goToLink('https://www.marketingdive.com/news/new-york-times-wordle-new-mobile-ad-format-doordash/685073/')}>
-                    <div className="title self-center w-2/5 pr-2">Wordle Mobile Ad</div>
-                    <div className="description self-center w-3/5 pl-3"> Supported the launch of a full-screen interstitial
-                    advertisement on The New York Times' Wordle game</div>
                 </div>
 
                 <div className="project cursor-pointer flex flex-row h-24 border-b-2 border-l-2 pl-4 border-black 
