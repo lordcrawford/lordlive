04d0fb0741e8e0aadd6ac03fdf516316f9ad2449
Lord Crawford <lrc2161@columbia.edu>
2024-11-06 00:39:47 -0500

    shows which page a user is on

--- src/components/righthalf/tab_space.js
+++ src/components/righthalf/tab_space.js
@@ -3,14 +3,36 @@ import './space.css';
 
 
 class TabSpace extends React.Component {
+    componentDidMount() {
+        let tabs = ['projects', 'art', 'about', 'writing']
+        for(const tab of tabs){
+            if (window.location.pathname === '/' + tab) {
+                const projectElement = document.querySelector('.' + tab + '.space');
+                if (projectElement) {
+                    projectElement.firstChild.classList.add('underline');
+                    projectElement.firstChild.style.fontWeight = '400';
+                }else{
+                    projectElement.firstChild.classList.remove('underline');
+                    projectElement.firstChild.style.fontWeight = '100';
+                }
+            }
+        }
+      }
+
     render (){
         function handleMouseOver(e) {
-            e.target.firstChild.style.color = 'black';
+            let tab_name = window.location.pathname.substring(1);
+            if(tab_name !== e.target.firstChild.textContent){
+                e.target.firstChild.style.color = 'black';
+            }
         }
 
         function handleMouseOut(e) {
+
             e.target.firstChild.style.color = 'white';
         }
+
+        
         
         const class_name = "w-1/3 h-1/5 " + this.props.pagename + " space";
         
